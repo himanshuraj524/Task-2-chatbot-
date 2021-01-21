c7fe0dab55d1a3d4b32ca9b70e8a2e92526c8185
Himanshu <himanshurajverma549@gmail.com>
2021-01-21 15:37:36 +0530

    Project completed

--- ChatBotApp/ChatBot/views.py
+++ ChatBotApp/ChatBot/views.py
@@ -57,6 +57,8 @@ def Response(request):
         Covidinp = ['what is covid', 'what is coronavirus',
                     'what is covid19', "covid", "covid19", "coronavirus", "corona"]  #covid related questions.
 
+        CovidPrecautions = ['what are the precautions of covid', 'precaution of covid', 'precautions'] #covid precaution related questions.
+
         if messageData in wishes:
             #wishes the user based on user message.
             BotReply = wishMe(messageData, wishes)
@@ -67,8 +69,23 @@ def Response(request):
 
         elif messageData in Covidinp:
             # if user asks about corona, user will get the below response.
-            BotReply = "A coronavirus is a kind of common virus that causes an infection in your nose, sinuses, or upper throat. Most coronaviruses aren't dangerous.In early 2020, after a December 2019 outbreak in China, the World Health Organization identified SARS-CoV-2 as a new type of coronavirus. The outbreak quickly spread around the world.COVID-19 is a disease caused by SARS-CoV-2 that can trigger what doctors call a respiratory tract infection. It can affect your upper respiratory tract (sinuses, nose, and throat) or lower respiratory tract (windpipe and lungs).It spreads the same way other coronaviruses do, mainly through person-to-person contact. Infections range from mild to deadly."
+            BotReply = """A coronavirus is a kind of common virus that causes an infection in your nose, sinuses, or upper throat. Most coronaviruses aren't dangerous.
+            In early 2020, after a December 2019 outbreak in China, the World Health Organization identified SARS-CoV-2 as a new type of coronavirus. The outbreak quickly spread around the world.COVID-19 is a disease caused by SARS-CoV-2 that can trigger what doctors call a respiratory tract infection.
+            It can affect your upper respiratory tract (sinuses, nose, and throat) or lower respiratory tract (windpipe and lungs).It spreads the same way other coronaviruses do, mainly through person-to-person contact. Infections range from mild to deadly."""
         
+        elif messageData in CovidPrecautions:
+            #if the user asks about the precautions of covid then he will get the response below.
+            BotReply = """To prevent the spread of COVID-19:
+            -Clean your hands often.
+            -Use soap and water, or an alcohol-based hand rub.
+            -Maintain a safe distance from anyone who is coughing or sneezing.
+            -Wear a mask when physical distancing is not possible.
+            -Don’t touch your eyes, nose or mouth.
+            -Cover your nose and mouth with your bent elbow or a tissue when you cough or sneeze.
+            -Stay home if you feel unwell.If you have a fever, cough and difficulty breathing, seek medical attention.-Calling in advance allows your healthcare provider to quickly direct you to the right health facility. This  protects you, and prevents the spread of viruses and other infections.
+            -Masks can help prevent the spread of the virus from the person wearing the mask to others. 
+            -Masks alone do not protect against COVID-19, and should be combined with physical distancing and hand hygiene"""
+            
         else:
             # if any error occured the this response will sended with the reply.
             BotReply = "Wrong input!"
